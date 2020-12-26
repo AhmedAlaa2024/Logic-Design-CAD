b14ac8f97775ad9cf5ba6437847f84f1c1331736
Ahmed-ata112 <72627749+Ahmed-ata112@users.noreply.github.com>
2020-12-26 19:28:08 +0200

    fixed the error

--- Actions/Action.h
+++ Actions/Action.h
@@ -8,21 +8,21 @@ class ApplicationManager; //forward class declaration
 class Action
 {
 protected:
-	ApplicationManager *pManager;	//Actions needs AppMngr to do their job
+	ApplicationManager* pManager;	//Actions needs AppMngr to do their job
 public:
-	Action(ApplicationManager *pApp) { pManager = pApp; }	//constructor
-	
+	Action(ApplicationManager* pApp) { pManager = pApp; }	//constructor
+	Action() {};
 	//Reads parameters required for action to execute
-	virtual void ReadActionParameters()=0;
-	
+	virtual void ReadActionParameters() = 0;
+
 	//Execute action (code depends on action type)
-	virtual void Execute()=0;
+	virtual void Execute() = 0;
 
 	//To undo this action (code depends on action type)
-	virtual void Undo()=0;
+	virtual void Undo() = 0;
 
 	//To redo this action (code depends on action type)
-	virtual void Redo()=0;
+	virtual void Redo() = 0;
 
 };
 
